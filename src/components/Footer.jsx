0c8fe321b6a4b2acc01e1b0b1ac87a72b7fabbd7
nylbix12 <159426396+nylbix12@users.noreply.github.com>
2025-07-10 12:26:15 +0200

    responsive accueil

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,44 +1,54 @@
+// src/components/Footer.jsx
 import { FaInstagram, FaSpotify, FaXTwitter } from "react-icons/fa6";
 import styled from "styled-components";
-
 
 const FooterWrapper = styled.div`
   position: fixed;
   bottom: 0;
   left: 0;
   width: 100%;
-  height: 30px;
   background-color: black;
   display: flex;
   justify-content: center;
   align-items: center;
   gap: 30px;
+  padding: 4px 0;
   z-index: 999;
+
+  @media (max-width: 768px) {
+    gap: 16px;
+    padding: 6px 0;
+  }
 `;
 
-
-const Icon = styled.div`
-  font-size: 26px;
+const Icon = styled.a`
+  display: flex;
   color: white;
-  cursor: pointer;
+  font-size: 26px;
+  transition: color 0.2s, transform 0.2s;
 
   &:hover {
     color: hotpink;
+    transform: scale(1.1);
+  }
+
+  @media (max-width: 768px) {
+    font-size: 20px;
   }
 `;
 
 export default function Footer() {
   return (
     <FooterWrapper>
-      <a href="https://instagram.com" target="_blank">
-        <Icon><FaInstagram /></Icon>
-      </a>
-      <a href="https://spotify.com" target="_blank">
-        <Icon><FaSpotify /></Icon>
-      </a>
-      <a href="https://twitter.com" target="_blank">
-        <Icon><FaXTwitter /></Icon>
-      </a>
+      <Icon href="https://instagram.com" target="_blank" aria-label="Instagram">
+        <FaInstagram />
+      </Icon>
+      <Icon href="https://spotify.com" target="_blank" aria-label="Spotify">
+        <FaSpotify />
+      </Icon>
+      <Icon href="https://twitter.com" target="_blank" aria-label="Twitter">
+        <FaXTwitter />
+      </Icon>
     </FooterWrapper>
   );
 }
