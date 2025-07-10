612575a073958920459c6253391b025da5e9ba5f
asmah <haddadasma006@gmail.com>
2025-07-10 12:12:21 +0200

    ajout contact

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -93,12 +93,12 @@ export default function ContactPage() {
         <LinkList>
           <li><StyledLink href="#">ARCADE FIRE</StyledLink></li>
           <li><StyledLink href="#">PINK ELEPHANT</StyledLink></li>
-          <li><StyledLink href="#">INSTAGRAM | TWITTER | FACEBOOK</StyledLink></li>
-          <li><StyledLink href="#">SPOTIFY</StyledLink></li>
-          <li><StyledLink href="#">DEEZER</StyledLink></li>
+          <li><StyledLink href="https://www.instagram.com/p/DItuwEKR9gM/">INSTAGRAM | TWITTER | FACEBOOK</StyledLink></li>
+          <li><StyledLink href="https://open.spotify.com/intl-fr/album/5qLTlCTZvN24Fm1nqd7XCV?si=qtHL-U7pSiKvnAPd5pQHWw&go=1&nd=1&dlsi=ab4dca1c7682427e">SPOTIFY</StyledLink></li>
+          <li><StyledLink href="https://www.deezer.com/fr/album/752544901?host=0&utm_campaign=clipboard-generic&utm_source=user_sharing&utm_content=album-752544901&deferredFl=1">DEEZER</StyledLink></li>
           <li><StyledLink href="#">APPLE MUSIQUE</StyledLink></li>
-          <li><StyledLink href="#">AMAZON MUSIQUE</StyledLink></li>
-          <li><StyledLink href="#">YOUTUBE MUSIQUE</StyledLink></li>
+          <li><StyledLink href="https://music.amazon.co.uk/albums/B0F18RBPB8?marketplaceId=A1F83G8C2ARO7P&musicTerritory=GB&tag=linkfiregen&ie=UTF8&linkCode=as2&ascsubtag=a3685dc481f1ecf214e877472315adb3&ref=dmm_acq_soc_fr_u_lfire_lp_x_a3685dc481f1ecf214e877472315adb3&fbclid=PAQ0xDSwLcamBleHRuA2FlbQIxMQABp342FGUqiV68p6LdsMOqUYz1mFvzR4TcOafLbCuvOqZ5FSUb9Dp1Z86XizeN_aem_QN2DGjdbHeNuLamN8csAjg">AMAZON MUSIQUE</StyledLink></li>
+          <li><StyledLink href="https://www.youtube.com/watch?v=0LpS5l5dRJ0&list=PLV_bVczPcRZRjDritaUGODWR3cv8_CBuZ&src=Linkfire&lId=e3ab07bb-74eb-4f52-935c-ea5f573a91c1&cId=d3d58fd7-4c47-11e6-9fd0-066c3e7a8751&fbclid=PAQ0xDSwLcamBleHRuA2FlbQIxMQABp342FGUqiV68p6LdsMOqUYz1mFvzR4TcOafLbCuvOqZ5FSUb9Dp1Z86XizeN_aem_QN2DGjdbHeNuLamN8csAjg">YOUTUBE MUSIQUE</StyledLink></li>
         </LinkList>
 
         <RestartButton to="/">RECOMMENCER</RestartButton>
